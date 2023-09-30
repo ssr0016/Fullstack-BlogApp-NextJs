183d1788930132e62fce0a1b3466d051f5e8e734
ssr0016 <samsonrecluta0016@gmail.com>
2023-09-30 22:00:25 +0800

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import styles from "./homepage.module.css";
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardlist/CardList";
+import Menu from "@/components/menu/Menu";
+
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Featured/>
+      <CategoryList/>
+      <div className={styles.content}>
+        <CardList/>
+        <Menu/>
+      </div>
+    </div>
+    
+
+  )
+}
