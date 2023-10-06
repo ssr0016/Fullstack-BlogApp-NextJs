b9e22c8eae4604824b5127470f8818c69356c614
ssr0016 <samsonrecluta0016@gmail.com>
2023-10-06 21:43:54 +0800

    adding some features in my blog app

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -14,7 +14,6 @@ const Menu = () => {
       <h2 className={styles.subtitle}>Discovery by topic</h2>
       <h1 className={styles.title}>Categories</h1>
       <MenuCategories/>
-
       <h2 className={styles.subtitle}>Chosen by the editor</h2>
       <h1 className={styles.title}>Editor Pick</h1>
       <MenuPosts withImage={true}/>
